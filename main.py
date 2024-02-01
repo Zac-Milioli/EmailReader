4f139b399ef5b7ed847a92e0d1cec747fb5d8a00
Zac-Milioli <zacmilioli@gmail.com>
2024-02-01 15:01:39 -0300

    totalmente funcional (eu acho)

--- main.py
+++ main.py
@@ -34,7 +34,6 @@ class EmailReader:
         chave_entry = ctk.CTkEntry(fm, placeholder_text='Título-chave', justify='center', width=400, height=60, font=fonte, text_color='black', fg_color='white', corner_radius=45, border_color='white', placeholder_text_color='black')
         chave_entry.pack(pady=common_pady)
 
-
         terminal_box = ctk.CTkTextbox(fm, fg_color='white', font=fonte_terminal, text_color='black', bg_color='white', border_color='white', height=120)
 
         def fetch_emails():
@@ -47,18 +46,10 @@ class EmailReader:
 
             email = MailBox('imap.gmail.com').login(user, password)
 
-            listagem_email = email.fetch(criteria=AND(seen=False), mark_seen=True, bulk=True)
+            listagem_email = email.fetch(criteria=AND(seen=False), mark_seen=True, bulk=True) 
 
-            tamanho = 0
             listagem_email = list(listagem_email)
-            for x in listagem_email:
-                if keyword != '':
-                    if keyword not in x.subject:
-                        listagem_email.remove(x)
-                    else:
-                        tamanho += 1
-                else:
-                    tamanho += 1
+            tamanho = len(listagem_email)
 
             terminal_box.insert('0.0', f'Returned {tamanho} emails\n')
             
@@ -67,14 +58,14 @@ class EmailReader:
                 for msg in listagem_email:
                     counter += 1
                     answer_mail = msg.subject.replace(keyword, '').replace(' ', '')
-                    terminal_box.insert('0.0', f'Processing mail from {answer_mail},\tnumber {counter} out of {tamanho}\n')
+                    terminal_box.insert('0.0', f'Processing mail from {answer_mail},\tnumber {counter}\n')
                     corpo = msg.text.replace("\'", "\"").replace('None', 'null').replace("\r", '').replace('\n', ' ').replace('\t', ' ')
-                    corpo = json.loads(corpo)
+                    corpo = json.loads(corpo, strict=False)
                     try:
                         df = pd.DataFrame(corpo)
                         df.to_csv(f'MAIL_{counter}.csv', sep=';')
                     except:
-                        terminal_box.insert('0.0', f'ERROR AT EMAIL FROM {answer_mail}. skipping...\n')
+                        terminal_box.insert('0.0', f'ERROR AT DATAFRAME FROM EMAIL FROM {answer_mail}. skipping...\n')
 
                 terminal_box.insert('0.0', 'Processing done\n')
 
@@ -84,8 +75,24 @@ class EmailReader:
 
         def join_dataframes():
             globed = glob('MAIL_*.csv')
-            print(globed)
-            terminal_box.insert('0.0', 'globed a few dataframes (or not)')
+            size_globed = len(globed)
+            if size_globed < 1:
+                terminal_box.insert('0.0', 'No DataFrames to join...\n')
+            else:
+                principal = pd.read_csv(globed[0], sep=';')
+                os.remove(globed[0])
+                globed.pop(0)
+                for i in globed:
+                    terminal_box.insert('0.0', f'Joining DataFrame {i} to the main\n')
+                    new = pd.read_csv(i, sep=';')
+                    principal = pd.concat([principal, new], ignore_index=True)
+                    os.remove(i)
+                main_already = glob('MAIN_DATAFRAME.csv')
+                if len(main_already) >= 1:
+                    principal.to_csv(f'MAIN_DATAFRAME_{len(main_already)}.csv', sep=';')
+                else:
+                    principal.to_csv('MAIN_DATAFRAME.csv', sep=';')
+            terminal_box.insert('0.0', 'Joining complete\n')
 
         joindf_button = ctk.CTkButton(fm, fg_color='#A9D18E', text='Juntar DataFrames', font=fonte, hover_color='#000000', text_color='white', corner_radius=45, width=240, height=50, command=join_dataframes)
         joindf_button.pack(pady=common_pady)
