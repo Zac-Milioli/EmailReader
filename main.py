e25499c9d50cd568222c20ca9eb7e537fe6e2913
Zac-Milioli <zacmilioli@gmail.com>
2024-02-02 11:32:47 -0300

    atualização

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import json
 from glob import glob
 import os
+from time import sleep
 from imap_tools import MailBox, AND
 
 fonte_t = ("Inter", 36, 'bold')
@@ -51,22 +52,26 @@ class EmailReader:
             listagem_email = list(listagem_email)
             tamanho = len(listagem_email)
 
-            terminal_box.insert('0.0', f'Returned {tamanho} emails\n')
+            terminal_box.insert('0.0', f'Returned {tamanho} objects\n')
             
             if tamanho != 0:
                 counter = 0
                 for msg in listagem_email:
-                    counter += 1
-                    answer_mail = msg.subject.replace(keyword, '').replace(' ', '')
-                    terminal_box.insert('0.0', f'Processing mail from {answer_mail},\tnumber {counter}\n')
-                    corpo = msg.text.replace("\'", "\"").replace('None', 'null').replace("\r", '').replace('\n', ' ').replace('\t', ' ')
-                    corpo = json.loads(corpo, strict=False)
-                    try:
-                        df = pd.DataFrame(corpo)
-                        df.to_csv(f'MAIL_{counter}.csv', sep=';')
-                    except:
-                        terminal_box.insert('0.0', f'ERROR AT DATAFRAME FROM EMAIL FROM {answer_mail}. skipping...\n')
-
+                    sleep(0.3)
+                    if len(msg.text) > 30:
+                        terminal_box.insert('0.0', f'Empty mail found. skipping...\n')
+                    else:
+                        counter += 1
+                        answer_mail = msg.subject.replace(keyword, '').replace(' ', '')
+                        terminal_box.insert('0.0', f'Processing mail from {answer_mail},\tnumber {counter}\n')
+                        corpo = msg.text.replace("\'", "\"").replace('None', 'null').replace("\r", '').replace('\n', ' ').replace('\t', ' ')
+                        corpo = json.loads(corpo, strict=False)
+                        try:
+                            df = pd.DataFrame(corpo)
+                            df.to_csv(f'MAIL_{counter}.csv', sep=';')
+                        except:
+                            terminal_box.insert('0.0', f'ERROR AT DATAFRAME FROM EMAIL FROM {answer_mail}. skipping...\n')
+                sleep(0.3)
                 terminal_box.insert('0.0', 'Processing done\n')
 
         fetch_button = ctk.CTkButton(fm, fg_color='#A9D18E', text='Fetch', font=fonte, hover_color='#000000', text_color='white', corner_radius=45, width=240, height=50, command=fetch_emails)
@@ -83,6 +88,7 @@ class EmailReader:
                 os.remove(globed[0])
                 globed.pop(0)
                 for i in globed:
+                    sleep(0.3)
                     terminal_box.insert('0.0', f'Joining DataFrame {i} to the main\n')
                     new = pd.read_csv(i, sep=';')
                     principal = pd.concat([principal, new], ignore_index=True)
@@ -92,6 +98,7 @@ class EmailReader:
                     principal.to_csv(f'MAIN_DATAFRAME_{len(main_already)}.csv', sep=';')
                 else:
                     principal.to_csv('MAIN_DATAFRAME.csv', sep=';')
+            sleep(0.3)
             terminal_box.insert('0.0', 'Joining complete\n')
 
         joindf_button = ctk.CTkButton(fm, fg_color='#A9D18E', text='Juntar DataFrames', font=fonte, hover_color='#000000', text_color='white', corner_radius=45, width=240, height=50, command=join_dataframes)
